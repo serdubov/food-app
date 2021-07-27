e2620c43770356381adef3addf595064b49c9c5d
sdubov <s.dubov3.14@gmail.com>
2021-07-27 22:26:29 +0300

    parallax

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import city from './public/city.png';
+import React, {useRef, useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [percentX, setPercentX] = useState('-100%')
+  const [percentY, setPercentY] = useState('-100%')
+
+  const onMouseMove = (e) => {
+    const {clientX, clientY, screenX, screenY, pageX, pageY} = e;
+    const {clientHeight, clientWidth} = e.target;
+    const targetCoords = e.target.getBoundingClientRect();
+   
+    const percentX = clientWidth / 100;
+    const percentY = clientHeight / 100;
+    const xCoord = clientX - targetCoords.left;
+    const yCoord = clientY - targetCoords.top;
+    setPercentX(`${xCoord / percentX}%`)
+    setPercentY(`${yCoord / percentY}%`)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="wrapper" onMouseMove={onMouseMove}>
+        <img className="glass-blur" src={'city.jpg'} alt='images'/>     
+        <div 
+          style={{
+            clipPath: `circle(20% at ${percentX} ${percentY})`
+          }}
+          className="container-mask"
         >
-          Learn React right now!!!
-        </a>
-      </header>
+          <img className="glass-image" src={'city.jpg'} alt='images'/>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
